f8fcbd3cf201b36a5b4a3128c4d3108f2f398145
Victor Beda <konbeda@gmail.com>
2025-05-17 10:46:08 -0300

    imgs update

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,5 +5,8 @@ export default defineConfig({
   build: {
     outDir: "../backend/frontend/dist",
   },
+  server: {
+    allowedHosts: ['95fb-177-138-87-192.ngrok-free.app'],
+  },
   plugins: [tailwindcss()],
 });
